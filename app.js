840109104b03b84335b83a43586dcce150b552b6
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-21 13:54:02 +0530

    async property added and commented for Educational Purposes.

--- app.js
+++ app.js
@@ -14,50 +14,51 @@ $(document).ready(function () {
         console.log(levelF);
 
         //create an object for accumilate data
-        const studentData = {
-            name: nameF,
-            city: cityF,
-            email: emailF,
-            level: levelF
-        };
-
-        console.log(studentData)
-
-        //create JSON
-        const studentJSON = JSON.stringify(studentData)
-        console.log(studentJSON)
-        // save the data with AJAX
-        const XMLReq = new XMLHttpRequest();
-        XMLReq.onreadystatechange = () => {
-            if (XMLReq.readyState == 4) {
-                if (XMLReq.status == 200 || XMLReq.status == 201) {
-                    var jsonTypeResponse = JSON.stringify(XMLReq.responseText)
-                    console.log(jsonTypeResponse)
-                } else {
-                    console.error("Error:", XMLReq.status, XMLReq.statusText)
-                }
-
-            } else {
-                console.log("Processing stage: Stage=", XMLReq.readyState);
-            }
-        }
-        XMLReq.open("POST", "http://localhost:8080/Stumgmt2024/student", true)
-        XMLReq.setRequestHeader("Content-Type", "application/json")
-        XMLReq.send(studentJSON)
-    });
-        // AJAX with JQuery
-        // $.ajax({
-        //     url: "http://localhost:8080/StudentManagemant2024_war/student",
-        //     type: "POST",
-        //     data: studentJSON,
-        //     headers: {"Content-Type": "application/json"},
-        //     success: (res) => {
-        //         console.log(JSON.stringify(res));
-        //     },
-        //     error: (res) => {
-        //         console.error(res);
-        //     }
-        // });
+    //     const studentData = {
+    //         name: nameF,
+    //         city: cityF,
+    //         email: emailF,
+    //         level: levelF
+    //     };
+    //
+    //     console.log(studentData)
+    //
+    //     //create JSON
+    //     const studentJSON = JSON.stringify(studentData)
+    //     console.log(studentJSON)
+    //     // save the data with AJAX
+    //     const XMLReq = new XMLHttpRequest();
+    //     XMLReq.onreadystatechange = () => {
+    //         if (XMLReq.readyState == 4) {
+    //             if (XMLReq.status == 200 || XMLReq.status == 201) {
+    //                 var jsonTypeResponse = JSON.stringify(XMLReq.responseText)
+    //                 console.log(jsonTypeResponse)
+    //             } else {
+    //                 console.error("Error:", XMLReq.status, XMLReq.statusText)
+    //             }
+    //
+    //         } else {
+    //             console.log("Processing stage: Stage=", XMLReq.readyState);
+    //         }
+    //     }
+    //     XMLReq.open("POST", "http://localhost:8080/Stumgmt2024/student", true)
+    //     XMLReq.setRequestHeader("Content-Type", "application/json")
+    //     XMLReq.send(studentJSON)
     // });
+        //AJAX with JQuery
+        $.ajax({
+            url: "http://localhost:8080/StudentManagemant2024_war/student",
+            type: "POST",
+            // async: false, //used to make ajax asynchronous
+            data: studentJSON,
+            headers: {"Content-Type": "application/json"},
+            success: (res) => {
+                console.log(JSON.stringify(res));
+            },
+            error: (res) => {
+                console.error(res);
+            }
+        });
+    });
 
 });
